e0655ce2ea5447b6ef02a7e305aead3f7555c828
spmadness <spmadness@gmail.com>
2023-11-15 17:49:09 +0300

    HW06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,9 @@
 package hw06pipelineexecution
 
+import (
+	"sync"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +13,63 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make(chan interface{})
+	wg := sync.WaitGroup{}
+	valChs := make([]chan interface{}, 0)
+
+	if in == nil || stages == nil {
+		close(out)
+		return out
+	}
+
+	for v := range in {
+		wg.Add(1)
+
+		// для каждого входного значения создаем канал и передаем его в горутину, для того,
+		// чтобы значения были на выходе в том же порядке, что и на входе.
+		valCh := make(chan interface{}, 1)
+		valChs = append(valChs, valCh)
+
+		go processStages(v, valCh, done, stages, &wg)
+	}
+
+	// ждем обработки всех значений, перебираем каналы со значениями в исходном порядке,
+	// освобождаем функцию ExecutePipeline
+	go waitAndSend(valChs, out, &wg)
+
+	return out
+}
+
+func processStages(v interface{}, valCh chan interface{}, done In, stages []Stage, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := range stages {
+		if stages[i] == nil {
+			continue
+		}
+		ch := make(chan interface{}, 1)
+		ch <- v
+		select {
+		case <-done:
+			close(ch)
+			close(valCh)
+			return
+		case v = <-stages[i](ch):
+			close(ch)
+		}
+	}
+	valCh <- v
+}
+
+func waitAndSend(valChs []chan interface{}, out chan interface{}, wg *sync.WaitGroup) {
+	wg.Wait()
+
+	for _, ch := range valChs {
+		if v, ok := <-ch; ok {
+			out <- v
+			close(ch)
+		}
+	}
+
+	close(out)
 }
