7d083d17a3c94faf491982ef2a1ffe5579d8d488
spmadness <spmadness@gmail.com>
2024-01-01 22:17:15 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,67 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const defaultDuration = "10s"
+
+var ErrWrongDurationArg = errors.New("wrong duration string value")
+
+var durationArg string
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.StringVar(&durationArg, "timeout", defaultDuration, "file to read from")
+	flag.Parse()
+
+	duration, err := time.ParseDuration(durationArg)
+	if err != nil {
+		fmt.Printf("duration arg parse error: %s\n", ErrWrongDurationArg)
+		os.Exit(1)
+	}
+
+	if len(flag.Args()) != 2 {
+		fmt.Println("go-telnet usage: go-envdir [--timeout=10s] host port")
+		os.Exit(1)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(),
+		os.Interrupt, os.Kill, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGPIPE)
+
+	address := net.JoinHostPort(flag.Args()[0], flag.Args()[1])
+
+	client := NewTelnetClient(address, duration, os.Stdin, os.Stdout)
+
+	err = client.Connect()
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("...connection error: %s\n", err))
+		return
+	}
+	defer client.Close()
+
+	go func() {
+		err = client.Send()
+		if err == nil {
+			os.Stderr.WriteString("...EOF\n")
+		}
+
+		cancel()
+	}()
+
+	go func() {
+		err = client.Receive()
+		if err == nil {
+			os.Stderr.WriteString("...Connection was closed by peer\n")
+		}
+	}()
+
+	<-ctx.Done()
 }
--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
 	"net"
+	"os"
 	"sync"
 	"testing"
 	"time"
@@ -61,5 +65,65 @@ func TestTelnetClient(t *testing.T) {
 		}()
 
 		wg.Wait()
+	})
+
+	t.Run("connection success", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+
+		client := NewTelnetClient(l.Addr().String(), time.Second*10, nil, nil)
+
+		stderr := os.Stderr
+		r, w, err := os.Pipe()
+		require.NoError(t, err)
+		defer r.Close()
+
+		os.Stderr = w
+
+		defer func() { os.Stderr = stderr }()
+
+		// Connect
+		err = client.Connect()
+		require.NoError(t, err)
+
+		scanner := bufio.NewScanner(r)
+		require.True(t, scanner.Scan(), "Failed to scan stderr: %v", scanner.Err())
+		actualMsg := scanner.Text()
+
+		expectedMsg := fmt.Sprintf("...Connected to %s", l.Addr().String())
+		require.Truef(t, actualMsg == expectedMsg,
+			"not equal, expectedMsg: %s, actualMsg: %s", expectedMsg, actualMsg)
+	})
+
+	t.Run("connection fail: non-existent socket address", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+
+		address := l.Addr().String()
+		l.Close()
+
+		client := NewTelnetClient(address, time.Second*10, nil, nil)
+
+		err = client.Connect()
+
+		opError := &net.OpError{}
+
+		require.ErrorAsf(t, err, &opError, "expected net.OpError actual error: %s", err)
+	})
+
+	t.Run("connection fail: timeout reached", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+
+		client := NewTelnetClient(l.Addr().String(), time.Microsecond*1, nil, nil)
+
+		err = client.Connect()
+		require.Error(t, err, "expected an error")
+
+		var netError net.Error
+		require.True(t, errors.As(err, &netError), "expected err to be of type net.Error")
+		require.True(t, netError.Timeout(), "expected err to be timeout error")
 	})
 }
